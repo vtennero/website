68d0c18b11d941328477645cb4cdc79f098c8659
vtennero <victor.tenneroni@gmail.com>
2024-04-25 01:15:22 +0800

    changed icon

--- app/page.js
+++ app/page.js
@@ -8,16 +8,18 @@ import AchievementsSections from "./components/AchievementsSections";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]  ">
-      <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        {/* <AchievementsSections /> */}
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col bg-[#121212]  ">
+        <Navbar />
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          {/* <AchievementsSections /> */}
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
